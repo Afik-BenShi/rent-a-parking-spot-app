7f46751497a64dcab9b051310f93362a148dd647
Afik-BenShi <afik@wix.com>
2024-02-04 21:14:21 +0200

    init

---
+++ pages/login-page.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function LoginPage(){
+    return <Text> Waiting for implementation </Text>
+}
---
+++ pages/offer-parking-page.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function OfferParking(){
+    return <Text> Waiting for implementation </Text>
+}
