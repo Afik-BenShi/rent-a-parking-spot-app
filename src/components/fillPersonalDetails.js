1e9ade4da2b30d6269c660345eeff59c54d4288b
einat gelbort <einatgelbort@gmail.com>
2024-05-21 19:33:15 +0300

    fix the nestedView using googleAutoComplete

--- src/components/fillPersonalDetails.js
+++ src/components/fillPersonalDetails.js
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
-import { ScrollView, View, Text, TouchableOpacity, SafeAreaView, StyleSheet, KeyboardAvoidingView, Platform } from 'react-native';
-// import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import { ScrollView, View, Text, TouchableOpacity, SafeAreaView } from 'react-native';
 import { Input } from '@rneui/themed';
 import MoreIcon from 'react-native-vector-icons/Ionicons';
 import Entypo from 'react-native-vector-icons/Entypo';
@@ -19,7 +18,7 @@ const FillPersonalDetails = ({ sendDataToParent, sendStartDateToParent, sendEndD
     const [startDate, setStartDate] = useState(new Date());
     const [endDate, setEndDate] = useState(new Date())
     const [imagePermissionsGranted, setImagePermissionsGranted] = useState(false);
-    const [showGoogleAutocomplete, setShowGoogleAutocomplete] = useState(true);
+    // const [showGoogleAutocomplete, setShowGoogleAutocomplete] = useState(true);
 
     const [endDateHasChanged, setEndDateHasChanged] = useState(false)
     const [startDateHasChanged, setStartDateHasChanged] = useState(false)
@@ -104,121 +103,119 @@ const FillPersonalDetails = ({ sendDataToParent, sendStartDateToParent, sendEndD
                 </View>
             </View>
 
-            <KeyboardAvoidingView
-                behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-                style={{ flex: 1 }}
-            >
-                <ScrollView keyboardShouldPersistTaps="handled">
-                    <View>
-                        <Input
-                            label="Product name"
-                            labelStyle={styles.inputLabel}
-                            placeholder=" Enter product name"
-                            onChangeText={(text) => sendDataToParent("productName", text)}
-                            inputStyle={styles.inputControl}
-                            inputContainerStyle={{ borderBottomWidth: 0 }}
+            <ScrollView keyboardShouldPersistTaps='handled' >
+                {/* listViewDisplayed={false} > */}
+                <View>
+                    <Input
+                        label="Product name"
+                        labelStyle={styles.inputLabel}
+                        placeholder=" Enter product name"
+                        onChangeText={(text) => sendDataToParent("productName", text)}
+                        inputStyle={styles.inputControl}
+                        inputContainerStyle={{ borderBottomWidth: 0 }}
+                    />
+
+                    <View style={{ flex: 1, padding: 20 }}>
+                        <Text style={{ ...styles.inputLabel, marginLeft: 0 }}>Product category</Text>
+                        <SingleSelectedDropDown
+                            onSelectCategory={sendCatToParent}
                         />
-
-                        <View style={{ flex: 1, padding: 20 }}>
-                            <Text style={{ ...styles.inputLabel, marginLeft: 0 }}>Product category</Text>
-                            <SingleSelectedDropDown
-                                onSelectCategory={sendCatToParent}
-                            />
-                        </View>
-
-                        {showGoogleAutocomplete && (<GooglePlacesAutocomplete
-                            placeholder="Enter your location"
-                            minLength={3} // minimum length of text to search
-                            fetchDetails={true}
-                            currentLocation={true}
-                            onPress={(data, details = null) => {
-                                console.log('GooglePlacesAutocomplete address:', details.geometry.location)
-                                sendDataToParent("address", details.geometry.location)
-                            }}
-                            onFail={error => console.log(error)}
-                            onNotFound={() => console.log('no results')}
-                            listEmptyComponent={() => (
-                                <View style={{ flex: 1 }}>
-                                    <Text>No results were found</Text>
-                                </View>
-                            )}
-                            query={{
-                                key: "fill_the_key_here",
-                                language: 'en', // language of the results
-                            }}
-                            styles={{
-                                textInputContainer: styles.googleInputContainer,
-                                textInput: styles.googleTextInput,
-                                predefinedPlacesDescription: {
-                                    color: '#1faadb'
-                                },
-                            }}
-                        />)}
-
-                        <Input
-                            label="Location"
-                            labelStyle={styles.inputLabel}
-                            leftIcon={<MoreIcon name="location-outline" size={18} />}
-                            placeholder=" Enter your location"
-                            onChangeText={(text) => sendDataToParent("city", text)}
-                            inputStyle={styles.inputControl}
-                            inputContainerStyle={{ borderBottomWidth: 0 }}
-                        />
-
-                        <Input
-                            label="Daily Price rate"
-                            labelStyle={styles.inputLabel}
-                            leftIcon={<Entypo color="#000" name="price-tag" size={16} />}
-                            placeholder=" Enter desired daily price"
-                            keyboardType="phone-pad"
-                            onChangeText={(text) => sendDataToParent("price", text)}
-                            inputStyle={styles.inputControl}
-                            inputContainerStyle={{ borderBottomWidth: 0 }}
-                        />
-
-                        <View style={styles.divider}>
-                            <View style={styles.dividerInset} /></View>
-
-                        <View>
-                            <View>
-                                <Text style={styles.sectionTitle}>Choose a range of available days </Text>
-                            </View>
-                        </View>
-
-                        <View style={styles.dateView}>
-                            <Text style={styles.datesLables}>  Start day : </Text>
-                            <DateTimePickerExample minDate={new Date()} onDateChange={onStartDateChange} />
-                        </View>
-                        <Text> </Text>
-                        <View style={styles.dateView}>
-                            <Text style={styles.datesLables}>  End day : </Text>
-                            <DateTimePickerExample minDate={startDate} onDateChange={onEndDateChange}
-                                valueToDisplay={valueInInvalidInput} />
-                        </View>
-
-                        <Text> </Text>
-
-                        <Input
-                            //multiline
-                            //numberOfLines={4}
-                            label="Description"
-                            labelStyle={styles.inputLabel}
-                            //leftIcon={<AnotherIcon name="coins" size={18} />}
-                            placeholder=" Enter product description"
-                            onChangeText={(text) => sendDataToParent("productDescription", text)}
-                            inputStyle={styles.inputControl}
-                            inputContainerStyle={{ borderBottomWidth: 0 }}
-                        />
-                        <TouchableOpacity style={styles.uploadImgButton} onPress={pickImage}>
-                            <Text style={styles.buttonText}>
-                                {<MaterialIcons color={COLORS.similarToBlack} name="file-upload" size={15} />}
-                                Upload image</Text>
-                        </TouchableOpacity>
                     </View>
 
-                </ScrollView>
-            </KeyboardAvoidingView>
-        </SafeAreaView>
+                    <GooglePlacesAutocomplete
+                        disableScroll={true}
+                        placeholder="Enter your location"
+                        minLength={3} // minimum length of text to search
+                        fetchDetails={true}
+                        returnKeyType={'default'}
+                        // currentLocation={true}
+                        onPress={(data, details = null) => {
+                            console.log('GooglePlacesAutocomplete address:', details.geometry.location)
+                            sendDataToParent("address", details.geometry.location)
+                        }}
+                        onFail={error => console.log(error)}
+                        onNotFound={() => console.log('no results')}
+                        // listEmptyComponent={() => (
+                        //     <View style={{ flex: 1 }}>
+                        //         <Text>No results were found</Text>
+                        //     </View>
+                        // )}
+                        query={{
+                            key: "AIzaSyDGMVgvnkeNr8urkP_YfBWStWKvWTbn4jk",
+                            language: 'en', // language of the results
+                        }}
+                        styles={{
+                            textInputContainer: styles.googleInputContainer,
+                            textInput: styles.googleTextInput,
+                            predefinedPlacesDescription: {
+                                color: '#1faadb'
+                            },
+                        }}
+                    />
+
+                    <Input
+                        label="Location"
+                        labelStyle={styles.inputLabel}
+                        leftIcon={<MoreIcon name="location-outline" size={18} />}
+                        placeholder=" Enter your location"
+                        onChangeText={(text) => sendDataToParent("city", text)}
+                        inputStyle={styles.inputControl}
+                        inputContainerStyle={{ borderBottomWidth: 0 }}
+                    />
+
+                    <Input
+                        label="Daily Price rate"
+                        labelStyle={styles.inputLabel}
+                        leftIcon={<Entypo color="#000" name="price-tag" size={16} />}
+                        placeholder=" Enter desired daily price"
+                        keyboardType="phone-pad"
+                        onChangeText={(text) => sendDataToParent("price", text)}
+                        inputStyle={styles.inputControl}
+                        inputContainerStyle={{ borderBottomWidth: 0 }}
+                    />
+
+                    <View style={styles.divider}>
+                        <View style={styles.dividerInset} /></View>
+
+                    <View>
+                        <View>
+                            <Text style={styles.sectionTitle}>Choose a range of available days </Text>
+                        </View>
+                    </View>
+
+                    <View style={styles.dateView}>
+                        <Text style={styles.datesLables}>  Start day : </Text>
+                        <DateTimePickerExample minDate={new Date()} onDateChange={onStartDateChange} />
+                    </View>
+                    <Text> </Text>
+                    <View style={styles.dateView}>
+                        <Text style={styles.datesLables}>  End day : </Text>
+                        <DateTimePickerExample minDate={startDate} onDateChange={onEndDateChange}
+                            valueToDisplay={valueInInvalidInput} />
+                    </View>
+
+                    <Text> </Text>
+
+                    <Input
+                        //multiline
+                        //numberOfLines={4}
+                        label="Description"
+                        labelStyle={styles.inputLabel}
+                        //leftIcon={<AnotherIcon name="coins" size={18} />}
+                        placeholder=" Enter product description"
+                        onChangeText={(text) => sendDataToParent("productDescription", text)}
+                        inputStyle={styles.inputControl}
+                        inputContainerStyle={{ borderBottomWidth: 0 }}
+                    />
+                    <TouchableOpacity style={styles.uploadImgButton} onPress={pickImage}>
+                        <Text style={styles.buttonText}>
+                            {<MaterialIcons color={COLORS.similarToBlack} name="file-upload" size={15} />}
+                            Upload image</Text>
+                    </TouchableOpacity>
+                </View>
+
+            </ScrollView>
+        </SafeAreaView >
 
 
     );
