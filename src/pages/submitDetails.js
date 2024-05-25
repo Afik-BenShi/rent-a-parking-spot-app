0147379feb1a9edeb7d904b47a98640ce00d9d5e
Noa Erez <noaerez@mail.tau.ac.il>
2024-05-25 08:29:43 +0300

    clean

--- src/pages/submitDetails.js
+++ src/pages/submitDetails.js
@@ -11,24 +11,21 @@ import {
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { COLORS } from '../../assets/theme';
 import NextBackBtn from '../components/nextAndBackBtn';
-import ExpandableImage from "../components/ExpandableImage";
+import ExpandableImage from '../components/ExpandableImage';
 
 import { getUser } from '../auth/auth';
-import config from '../backend/config'
+import config from '../backend/config';
 import { RefreshContext } from '../context/context';
 import { uploadImage, convertToBytes } from '../utils/imageStorage';
 
-const defaultImage = require("../../assets/parking-details-images/placeholder.png");
-
-
-
+const defaultImage = require('../../assets/parking-details-images/placeholder.png');
 
 export default function SubmitDetails({ navigation, route }) {
   const { refresh, setRefresh } = useContext(RefreshContext);
   const { detailsList, user } = route.params;
   const [loading, setLoading] = useState(false);
   const [userId, setUserId] = useState(user);
-  console.log("userId in submitDetails: ", userId);
+  console.log('userId in submitDetails: ', userId);
 
   const onGoBackPress = () => {
     navigation.goBack();
@@ -41,91 +38,60 @@ export default function SubmitDetails({ navigation, route }) {
     { key: '3', value: 'Home Improvement' },
   ];
 
-  const categoryName = data.find((item) => item.key === detailsList.category) ?
-    data.find((item) => item.key === detailsList.category).value : "";
+  const categoryName = data.find((item) => item.key === detailsList.category)
+    ? data.find((item) => item.key === detailsList.category).value
+    : '';
 
-  // --------------------------------------------------------
   // Function to handle the Finish button click
-  const onClickFinish = async ({ navigation, detailsList, userId, refresh, setRefresh }) => {
-    console.log("Product details submitted: ", detailsList);
-  
+  const onClickFinish = async () => {
+    console.log('Product details submitted: ', detailsList);
+
     // Send the details to the backend
-    // Send a POST request to your server
     let token;
     try {
       token = await getUser()?.getIdToken();
     } catch (error) {
-      console.error("Error getting user token:", error);
-      // Handle the error appropriately
+      console.error('Error getting user token:', error);
       return;
     }
-  
+
     let imageUrl;
     try {
       const storagePath = `images/${userId}-product-${encodeURI(detailsList.productName)}`;
-      // Assuming detailsList.imageUri contains the local path to the image
-      imageUrl = await uploadImage(storagePath, detailsList.imageUri); 
+      imageUrl = await uploadImage(storagePath, detailsList.imageUri);
       console.log('Firebase Storage Image URL:', imageUrl);
-  
-      //const path = `images/${userId}-product-${encodeURI(detailsList.productName)}`;
-      try{
-          const imageBlob = await convertToBytes(imageUrl);
-          console.log('Image blob created:', imageBlob);
-  
-          // const formData = new FormData();
-          // formData.append('title', detailsList.productName);
-          // formData.append('image', imageBlob);
-  
-          // post product int the db:
-          const urlFromFirebase = imageUrl;
-          await postNewProduct(urlFromFirebase, detailsList);
-          
-          // Navigate to the My Products page
-          // Use CONTEXT - to remove the Non-seriazable warning
-          //setRefresh(true);
-          
-          setTimeout(() => setRefresh(true), 2);
-          setRefresh(true);
-          setTimeout(() => setRefresh(false), 3);
-          //setLoading(false);
-          navigation.navigate("My Products cardList");
-  
+
+      try {
+        const imageBlob = await convertToBytes(imageUrl);
+        console.log('Image blob created:', imageBlob);
+
+        const urlFromFirebase = imageUrl;
+        await postNewProduct(urlFromFirebase, detailsList);
+
+        setTimeout(() => setRefresh(true), 2);
+        setRefresh(true);
+        setTimeout(() => setRefresh(false), 3);
+        navigation.navigate('My Products cardList');
+      } catch (err) {
+        console.error('Error while converting image to blob:', JSON.stringify(err));
       }
-      catch (err) {
-        console.error("Error while converting image to blob:", JSON.stringify(err));
-      }
-      
-      
-      // imgRes = await fetch(`http://${config.serverIp}:${config.port}/myProducts/img`, {
-      //   method: 'POST',
-      //   headers: {
-      //     Authorization: token
-      //   },
-      //   body: formData
-      // });
-      
-    }
-    catch (err) {
-      console.error("Error while uploading an image:", JSON.stringify(err));
+    } catch (err) {
+      console.error('Error while uploading an image:', JSON.stringify(err));
       return;
-    
-    //imgRes = await imgRes?.json();  
+    }
   };
-
-
 
   const postNewProduct = async (urlFromFirebase, detailsList) => {
     let token;
     try {
       token = await getUser()?.getIdToken();
     } catch (error) {
-      console.error("Error getting user token:", error);
-      // Handle the error appropriately
+      console.error('Error getting user token:', error);
       return;
     }
 
-    console.log("url from firebase in post new product :", urlFromFirebase);
-    
+    console.log('url from firebase in post new product :', urlFromFirebase);
+
     const newProduct = {
       title: detailsList.productName,
       pricePerDay: detailsList.price,
@@ -135,115 +101,91 @@ export default function SubmitDetails({ navigation, route }) {
       fromDate: new Date(detailsList.fromDate),
       untilDate: new Date(detailsList.untilDate),
       address: detailsList.address,
-      //imageName: imgRes?.imageName,
-      imageUrl: urlFromFirebase,  // url to firebase storage
+      imageUrl: urlFromFirebase,
     };
 
-    console.log('newProduct', newProduct)
+    console.log('newProduct', newProduct);
 
     fetch(`http://${config.serverIp}:${config.port}/myProducts/add`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        Authorization: token
+        Authorization: token,
       },
-      body: JSON.stringify(newProduct)
+      body: JSON.stringify(newProduct),
     })
-      .then(response => {
-        console.log("success to post new product")
-        console.log("Response from server:", response.ok);
+      .then((response) => {
+        console.log('success to post new product');
+        console.log('Response from server:', response.ok);
       })
-      .catch(error => {
-        console.error("Error while posting new product:", JSON.stringify(error));
+      .catch((error) => {
+        console.error('Error while posting new product:', JSON.stringify(error));
       });
-
   };
-
-
 
   return (
     <View style={{ flex: 1 }}>
       <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
         <View style={styles.container}>
           <View style={styles.header}>
-            <View style={styles.headerAction}>
-            </View>
+            <View style={styles.headerAction}></View>
             <Text style={styles.title}>Your product</Text>
-            <View
-              style={[styles.headerAction, { alignItems: 'flex-end' }]} />
+            <View style={[styles.headerAction, { alignItems: 'flex-end' }]} />
           </View>
 
-          <ScrollView
-            contentContainerStyle={styles.receipt}
-            showsVerticalScrollIndicator={false}>
-
+          <ScrollView contentContainerStyle={styles.receipt} showsVerticalScrollIndicator={false}>
             <View style={styles.receiptLogo}>
-              {<Icon color={COLORS.lightWhite} name="account-details" size={32} />}
+              <Icon color={COLORS.lightWhite} name="account-details" size={32} />
             </View>
 
-
-
-            {/*******/}
             <View style={styles.divider}>
-              <View style={styles.dividerInset} /></View>
+              <View style={styles.dividerInset} />
+            </View>
 
             <View style={styles.details}>
               <Text style={styles.detailsTitle}>Product details</Text>
 
               <View style={styles.detailsRow}>
                 <Text style={styles.detailsField}>Product name</Text>
-
                 <Text style={styles.detailsValue}>{detailsList.productName}</Text>
               </View>
 
               <View style={styles.detailsRow}>
                 <Text style={styles.detailsField}>Location</Text>
-
                 <Text style={styles.detailsValue}>{detailsList.city}</Text>
               </View>
 
               <View style={styles.detailsRow}>
                 <Text style={styles.detailsField}>Category</Text>
-
-                <View>
-                  <Text style={styles.detailsValue}>{categoryName}</Text>
-                </View>
-
+                <Text style={styles.detailsValue}>{categoryName}</Text>
               </View>
-
 
               <View style={styles.detailsRow}>
                 <Text style={styles.detailsField}>Daily Price</Text>
-
-                <Text style={styles.detailsValue}>{detailsList.price}
-                  <Icon name="currency-ils" size={14} color="#000" /> / day</Text>
+                <Text style={styles.detailsValue}>
+                  {detailsList.price}
+                  <Icon name="currency-ils" size={14} color="#000" /> / day
+                </Text>
               </View>
 
               <View style={styles.detailsRow}>
                 <Text style={styles.detailsField}>Days of availability</Text>
-
                 <Text style={styles.detailsValue}>
                   {detailsList.from} - {detailsList.until}
                 </Text>
               </View>
 
-              <View >
-                <Text style={styles.detailsField}>
-                  {'\n'}Description: </Text>
-
-                <Text style={styles.descriptionValue}>
-                  {'\n'}{detailsList.productDescription}
-                </Text>
+              <View>
+                <Text style={styles.detailsField}>{'\n'}Description: </Text>
+                <Text style={styles.descriptionValue}>{'\n'}{detailsList.productDescription}</Text>
               </View>
 
               <View>
                 <Text style={styles.detailsField}>Image</Text>
                 <ExpandableImage source={productImage} initialHeight={250} />
               </View>
-
             </View>
           </ScrollView>
-
         </View>
       </SafeAreaView>
 
@@ -258,22 +200,18 @@ export default function SubmitDetails({ navigation, route }) {
           nextText="Finish"
           backText="Back"
           navigation={navigation}
-          onNextPress={() => { /*setLoading(true);*/
-            onClickFinish({ navigation, detailsList, userId, refresh, setRefresh });
+          onNextPress={() => {
+            onClickFinish();
           }}
           paddingBottom={0}
         />
       </View>
 
-    {/* {loading && (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <ActivityIndicator size="large" color="#0000ff" />
-      </View>
-    )} */}
-    
-  
-
-
+      {loading && (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <ActivityIndicator size="large" color="#0000ff" />
+        </View>
+      )}
     </View>
   );
 }
@@ -307,7 +245,6 @@ const styles = StyleSheet.create({
     shadowRadius: 2.22,
     elevation: 3,
   },
-  /** Header */
   header: {
     flexDirection: 'row',
     alignItems: 'center',
@@ -332,7 +269,6 @@ const styles = StyleSheet.create({
     marginTop: 15,
     textAlign: 'center',
   },
-  /** Receipt */
   receipt: {
     alignItems: 'center',
     paddingTop: 16,
@@ -364,7 +300,6 @@ const styles = StyleSheet.create({
     marginBottom: 14,
     flexDirection: 'row',
   },
-  /** Divider */
   divider: {
     overflow: 'hidden',
     width: '100%',
@@ -377,7 +312,6 @@ const styles = StyleSheet.create({
     borderStyle: 'dashed',
     marginTop: -2,
   },
-  /** Details */
   details: {
     width: '100%',
     flexDirection: 'column',
@@ -414,7 +348,6 @@ const styles = StyleSheet.create({
     flexBasis: 0,
     textAlign: 'right',
   },
-  /** Button */
   btn: {
     flexDirection: 'row',
     alignItems: 'center',
