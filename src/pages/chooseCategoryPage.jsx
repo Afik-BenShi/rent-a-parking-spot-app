f66d5d16915ee6a52bba3a9ee9755dd974b3d9ae
Afik-BenShi <afik@wix.com>
2024-05-28 20:08:53 +0300

    fixes for deployment

--- src/pages/chooseCategoryPage.jsx
+++ src/pages/chooseCategoryPage.jsx
@@ -13,7 +13,7 @@ import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
 const items = [
   {
-    img: 'https://cdn.gencraft.com/prod/user/a275d62c-6004-4894-acc7-a4bdf104d297/9f76b2de-6b4e-4f3b-8346-ad04cc56a5a8/image/image0_0.jpg?Expires=1715173724&Signature=CqxXEjPEigAar1VGdvhGOQEIdP4SKdRI6s7-xBgn~sZPbaYx-5HnUAGXxN8E~LKvEdTDJ086aUu0yuZ-yw6CPpKJXkR1Ec1cG6mcQrn4P~-pdEzeOXx1AIxgABUU~5rr0-HQuTl5wNjHCPuQvuyYFXiIPctys9Id4xxfkecHMxXCtflahApUo1PhU8iQ7-7tDdE3meOY4yW754c0aFo5TQRzJl7pBXBibhlhBMzId1VNOFmMZ3pp4DKtO6A7YHboMKtEaakSNEF-t-Pj7BOC7NVChWCwdM4ZPLGSQMycS5uOLP3aZb-V~XmyLF~q7Tg~4OtQzwVNtgazx~q1BDriTg__&Key-Pair-Id=K3RDDB1TZ8BHT8',
+    img: 'https://firebasestorage.googleapis.com/v0/b/rentalwize-481c2.appspot.com/o/assets%2Fall.jpeg?alt=media&token=6d1e427f-8cfa-4d7a-ba5d-7c68d9650e72',
     title: 'View All',
     
     tag: '',
@@ -21,23 +21,23 @@ const items = [
 
   },
   {
-    img: 'https://cdn.gencraft.com/prod/user/7c5b0398-eaa9-42fa-8441-322697c5bb2f/5c6bffde-ae95-4973-b072-9d7c55be6730/image/image0_0.jpg?Expires=1715173060&Signature=J40bDZtTupgco5nPKOSuOxz4j3LcC4Omsd0vEqt1xw6IWN2Vw3TnZou5rRIW7frkBM9YZvjs5hQ2qyUrIqcAXgAeWQqK8sOpqCoGlDlR3uLvgLpmrKXDSdwfVnlef5aGesPrrRgyFr8K2uiIUiaxxRkWDr6V-XOFPmsrFPSSLclzZxqNwIN-VsZQysX75lddXY~VBWt76rqb7a-mDxz4C04iD5ElnOQ6851L~zjhCJc1Fsf6vi9QAdH55Oc2K16SrLo0eJcQdAsy1-4~GmxMRU8kVSD~xTAmeZp7jz1FSSCgGHhXZXxQcTGdkKcqdKlKiKqA-OgqvaU7Vfk39jRgRw__&Key-Pair-Id=K3RDDB1TZ8BHT8',
+    img: 'https://firebasestorage.googleapis.com/v0/b/rentalwize-481c2.appspot.com/o/assets%2Foutdoor.jpeg?alt=media&token=608a24e5-d5d7-477a-8fd5-6ad1b5b294a5',
     title: 'Outdoor Equipment',
     
-    tag: '· Camping Equipment: Tents, sleeping bags, camping stoves, portable chairs.\n\n· Sports Gear: Bikes, surfboards, kayaks, and outdoor games.',
+    tag: 'Camping Equipment: Tents, sleeping bags, camping stoves, portable chairs.\nSports Gear: Bikes, surfboards, kayaks, and outdoor games.',
     idx: "1",
   },
   {
-    img: 'https://cdn.gencraft.com/prod/user/a275d62c-6004-4894-acc7-a4bdf104d297/1972c14c-d454-4eb7-a157-ffdc8eb2a226/image/image1_0.jpg?Expires=1715173471&Signature=EJX2Q7dM6gTY8k2D7lKNHLY4Ct0M4ME2rEAGN1xK1SRdIOgPbTm7Vx2gIh6oDR3m3WZ~e1AU4UYdNooiU2-ZLiimxn2yiVz2i96Dr8dcqefhVTGV9P0qtzvaQ52Et-EMGtV08XkC0BM732RbY~Rr0SbeawFMhvS~nK6yO50X-gxO3zR3eS5~rTqSLIuLG63GLXxnN4DBgM6aqa0Zy2MlhoJnldeAHIuIHkh5krQAYzfDq8BTcq5Rnxkb9t3Jz2IaLQA4ap6xMUY30XF60VjZveZ10g0RWSfGWTjihWbeG5prSaKZCkOmtN28zludebDivIBdDCiMwt644VPFzlBqZg__&Key-Pair-Id=K3RDDB1TZ8BHT8',
+    img: 'https://firebasestorage.googleapis.com/v0/b/rentalwize-481c2.appspot.com/o/assets%2Fevents.jpeg?alt=media&token=041c7210-164a-4a8e-9e0c-20aa540f9856',
     title: 'Entertainment & Events',
-    tag: '· Speakers and Sound Systems Event Decor: Chairs, tables, party lights, event tents.',
+    tag: 'Speakers and Sound Systems Event Decor: Chairs, tables, party lights, event tents.',
     idx: "2",
   },
   {
-    img: 'https://images.unsplash.com/photo-1565615833231-e8c91a38a012?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80',
+    img: 'https://firebasestorage.googleapis.com/v0/b/rentalwize-481c2.appspot.com/o/assets%2Fhome-improvement.jpeg?alt=media&token=dd90d5c1-4a01-4f31-9a7d-42ca8544bf1f',
     title: "Home Improvement",
     
-    tag: '· Power Tools: Drills, saws, and other power tools for DIY projects.\n\n· Garden Equipment: Lawnmowers, trimmers, and gardening tools.',
+    tag: 'Power Tools: Drills, saws, and other power tools for DIY projects.\nGarden Equipment: Lawnmowers, trimmers, and gardening tools.',
     idx: "3",
   },
 ];
@@ -64,14 +64,13 @@ export default function ChooseCategory({ navigation }) {
             <Text style={styles.buttonText}>Get Started</Text>
         </TouchableOpacity> */}
 
-        <Text style={styles.title}>CATEGORIES</Text>
+        <Text style={styles.catTitle}>CATEGORIES</Text>
 
         {items.map(({ img, title, tag, idx }, index) => {
           return (
             <TouchableOpacity
             key={idx}
             onPress={() => {
-              console.log('Pressed item index:', idx); 
               navigation.navigate('HomeCard', {category:idx});
             }}>
               <View style={styles.card}>
@@ -107,11 +106,18 @@ const styles = StyleSheet.create({
     paddingBottom:130,
   },
   title: {
+    fontSize: 35,
+    fontWeight: '700',
+    color: '#173153',
+    marginTop:20,
+    alignSelf:'center',
+  },
+  catTitle: {
     fontSize: 20,
     fontWeight: '700',
     color: '#173153',
-    marginLeft: 25,
     marginTop:10,
+    marginBottom:10,
   },
   /** Card */
   card: {
@@ -188,8 +194,7 @@ const styles = StyleSheet.create({
   },
   subtitle: {
     fontSize: 18,
-    marginBottom: 30,
-    textAlign: 'center',
+    marginTop: 30,
   },
   button: {
     backgroundColor: '#007bff',
